531fa3c5f39433e076608bda90b944e712515e53
Daniel Barros <barros.daniel@outlook.com>
2019-07-08 08:34:41 +0800

    Complete exercise 1

--- 1_create_array.js
+++ 1_create_array.js
@@ -7,24 +7,46 @@ You can clear the console by typing `clear` and pressing enter.
 If your program is stuck in an infinite loop, you can break out of the program by typing ctrl + C.
 */
 
-// 1. Create a new array named languages that contains at least 7 programming languages.
+//DONE 1. Create a new array named languages that contains at least 7 programming languages.
+var languages = [
+  'JavaScript',
+  'Python',
+  'C#',
+  'Ruby',
+  'Scala',
+  'C++',
+  'Java',
+];
 
-// 2. Use console.log to display the number of elements in the array:
+//DONE 2. Use console.log to display the number of elements in the array:
+console.log(languages.length);
 
-/* 3. Use console.log to list the first element from the array using an index value. */
+/*DONE 3. Use console.log to list the first element from the array using an index value. */
+console.log(languages[0]);
 
-/* 4. Use console.log to list the last element from the array using an index value. */
+/*DONE 4. Use console.log to list the last element from the array using an index value. */
+console.log(languages[languages.length - 1]);
 
-// 5. Use an array method to add an element to the END of the array.
+//DONE 5. Use an array method to add an element to the END of the array.
+languages.push('C');
+console.log(languages);
 
-// 6. Use an array method to add an element to the BEGINNING of the array.
+//DONE 6. Use an array method to add an element to the BEGINNING of the array.
+languages.unshift('Kotlin');
+console.log(languages);
 
-/* 7. Log all of the elements in the array as a single string using the array .join() method */
+/*DONE 7. Log all of the elements in the array as a single string using the array .join() method */
+console.log(languages.join(', '));
 
-// 8. Use an array method to REMOVE the LAST element from the array.
+//DONE 8. Use an array method to REMOVE the LAST element from the array.
+languages.pop();
+console.log(languages);
 
-// 9. Use an array method to REMOVE the first element from the array.
+//DONE 9. Use an array method to REMOVE the first element from the array.
+languages.shift();
+console.log(languages);
 
-/* 10. Log all of the elements in the array as a single string using the array .join() method. Thisis the same as step 7 above */
+/*DONE 10. Log all of the elements in the array as a single string using the array .join() method. Thisis the same as step 7 above */
+console.log(languages.join(', '));
 
 // Run your code by typing node 1_create_array.js in the console below
