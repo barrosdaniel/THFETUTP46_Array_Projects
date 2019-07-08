5ab8b3ef6b781513f1510ecfc23c3a168163fa8d
Daniel Barros <barros.daniel@outlook.com>
2019-07-08 08:44:41 +0800

    Complete exercise 2

--- 2_loop.js
+++ 2_loop.js
@@ -12,13 +12,28 @@ function random100() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-/* 1. Create a function named createRandomList that uses a for loop to create an array containing 10 random numbers from 1 to 100 (use the supplied function above to generate the numbers). The function should return that array. */
+/*DONE 1. Create a function named createRandomList that uses a for loop to create an array containing 10 random numbers from 1 to 100 (use the supplied function above to generate the numbers). The function should return that array. */
+var myRandomList;
 
-/* 2. Call the createRandomList() function and store the results in a variable named myRandomList. */
+function createRandomList() {
+  var number;
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    number = random100();
+    array.push(number);
+  }
+  return array;
+}
 
-/* 3. Use a for loop to access each element in the loop. Each time through the loop log a message to the console that looks something like this:
+/*DONE 2. Call the createRandomList() function and store the results in a variable named myRandomList. */
+myRandomList = createRandomList();
+
+/*DONE 3. Use a for loop to access each element in the loop. Each time through the loop log a message to the console that looks something like this:
 Item 0 in the array is 48
 When you're done you should have output 10 lines to the console -- one for each element. 
 */
+for (var i = 0; i < myRandomList.length; i++) {
+  console.log('Item ' + i + ' in the array is ' + myRandomList[i]);
+}
 
 // Run your code by typing node 2_loop.js in the console below
